f4cdacfb67a46df33574e2cc1e397b438aed7ba1
ragnhlar <ragnhlar@stud.ntnu.no>
2018-05-29 13:56:12 +0200

    Added comment to Call to arms activity

--- app/src/main/java/com/example/desent/desent/activities/CallToArmsActivity.java
+++ app/src/main/java/com/example/desent/desent/activities/CallToArmsActivity.java
@@ -36,7 +36,6 @@ public class CallToArmsActivity extends AppCompatActivity implements NavigationV
 
     DrawerLayout drawer;
 
-    ChallengeAdapter adapter;
     List<Challenge> challengeList;
     RecyclerView recyclerView;
 
@@ -66,6 +65,7 @@ public class CallToArmsActivity extends AppCompatActivity implements NavigationV
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please wait...");
 
+        //hardcoded challenges - this must be removed when connecting to a server
         challengeList.add(new Challenge(1, "Invite Friends", "Invite 10 new friends to start using the app.", 7, "40"));
         challengeList.add(new Challenge(2, "Car-Free Weekend", "Leave the car unused for the weekend. The air quality is poor at the moment, and driving less will help improve air quality.", 2, "20"));
         challengeList.add(new Challenge(3, "Car-Free Work Week", "Leave the car unused during the work week. Take the bike or walk instead. This will give you health benefits as well as saving you both money and the environment.", 5, "50"));
